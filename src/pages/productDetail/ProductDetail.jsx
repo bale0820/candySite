f60dd655fe4dfbded7ee01643a65a1c3a7a1d23b
shyun <exriik@gmail.com>
2025-12-03 16:14:44 +0900

    Add gradlew wrapper files

--- src/pages/productDetail/ProductDetail.jsx
+++ src/pages/productDetail/ProductDetail.jsx
@@ -30,6 +30,7 @@ export function ProductDetail() {
   const dispatch = useDispatch();
   const product = useSelector((state) => state.product.product);
 
+  console.log("product", product);
   // product 최신화
   useEffect(() => {
     const fetchData = async () => {
