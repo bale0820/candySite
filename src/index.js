e6e9c516610d445f5c51180dda4bf481bcc96c51
unknown <dbsgywls667@gmail.com>
2025-12-07 16:05:56 +0900

    세션토큰을 이용한 구현성공

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-import React from 'react';
+import React from "react";
 import { Provider } from "react-redux";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import { store } from "./app/store.js";
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './styles/main.scss';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./styles/main.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-// store는 프로퍼티 이름 고정
-  <Provider store={ store }>  
-    {/* <React.StrictMode> */}
+  // store는 프로퍼티 이름 고정
+    <Provider store={store}>
       <App />
-    {/* </React.StrictMode> */}
-  </Provider>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
